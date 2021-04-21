f540a05da50f9247c8e115977503e36a87ccd3eb
OnSebii <lang.sebastian.006@gmail.com>
2021-04-21 09:44:53 +0200

    Express Sessions 2

---
+++ Arbeitsblatt - Express Sessions 2 - Session mit Vue CLI und Router/Express Sessions - Vue CLI - START/server/routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const users = require('../model/users.js');
+
+// enter your code here
+router.post('/login', (req, res) => {
+  console.log(req);
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email && password) {
+    const user = users.find((el) => el.email === email && el.password === password);
+    if (user) {
+      req.session.userId = user.id;
+      res.status(200).json({ id: user.id, name: user.name });
+    } else res.status(401).send('Wrong email or password');
+  } else res.status(400).send('Login failed');
+});
+
+// router.get('/logout', redirectLogin, (req, res) => {
+//   // enter your code here
+// });
+
+router.post('/register', (req, res) => {
+  // enter your code here
+});
+
+router.get('/secretdata', (req, res) => {
+  // enter your code here
+});
+
+module.exports = router;
